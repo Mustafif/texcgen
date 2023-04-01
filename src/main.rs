07fb3db7259db959a2d2a9275e0fe0ac05331043
Mustafif <mustafif0929@gmail.com>
2023-04-01 17:34:31 -0400

    added refresh command

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ mod template;
 
 use crate::builder::{generate, generate_all, save, Builder};
 use structopt::StructOpt;
-use tokio::io::Result;
+use tokio::{io::{Result, AsyncWriteExt}, fs::{File, read_to_string}};
 
 #[derive(StructOpt)]
 #[structopt(about = "A template generator for the TexCreate project")]
 enum CLI {
     #[structopt(about = "Initialize Output Directory Structure")]
     Init,
+    #[structopt(about = "Refreshes `src/template.rs` using the default template.")]
+    Refresh, 
     #[structopt(about = "Generate template")]
     Gen {
         #[structopt(short, long)]
@@ -34,6 +36,12 @@ async fn main() -> Result<()> {
 
     match cli {
         CLI::Init => Builder::default().create_dirs().await?,
+        CLI::Refresh => {
+            println!("Refreshing default template...");
+            let mut template = File::create("src/template.rs").await?;
+            let s = read_to_string("default/default_template.rs").await?;
+            template.write_all(s.as_bytes()).await?;
+        }
         CLI::Gen { level } => {
             let builder = match level {
                 None => Builder::default(),
